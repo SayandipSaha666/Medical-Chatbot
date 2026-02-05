505e0310acd095a2ba60e348a3c5276e809f83cd
SayandipSaha666 <sahasayandip480@gmail.com>
2026-02-06 02:49:27 +0530

    changes

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,20 +6,20 @@ class Settings(BaseSettings):
     groq_api_key: str
     huggingfacehub_access_token: str
     port: int
-    database_username: str
-    database_password: str
-    database_hostname: str
-    database_port: int
-    database_name: str
+    database_username: Optional[str] = None
+    database_password: Optional[str] = None
+    database_hostname: Optional[str] = None
+    database_port: Optional[int] = None
+    database_name: Optional[str] = None
+    sqlalchemy_database_url: Optional[str] = None
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
-    sqlalchemy_database_url: Optional[str] = None
-    clerk_jwks_url: str
-    clerk_issuer: str
-    imagekit_url_endpoint: str
-    imagekit_public_key: str
-    imagekit_private_key: str
+    clerk_jwks_url: Optional[str] = None
+    clerk_issuer: Optional[str] = None
+    imagekit_url_endpoint: Optional[str] = None
+    imagekit_public_key: Optional[str] = None
+    imagekit_private_key: Optional[str] = None
 
     class Config:
         env_file = ".env"
--- backend/app/database.py
+++ backend/app/database.py
@@ -3,15 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 from .config import settings
 from urllib.parse import quote
+import models # To automatically define database in neondb whenever this file is run.
+# encoded_password = quote(settings.database_password)
 
-encoded_password = quote(settings.database_password)
+# SQLALCHEMY_DATABASE_URL = (
+#     f"postgresql://{settings.database_username}:{encoded_password}"
+#     f"@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# )
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{settings.database_username}:{encoded_password}"
-    f"@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-)
-
-# SQLALCHEMY_DATABASE_URL=settings.sqlalchemy_database_url
+SQLALCHEMY_DATABASE_URL=settings.sqlalchemy_database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
